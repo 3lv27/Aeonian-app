cfa64195caac6d6481ba9e0fa84bef35d52a81ff
3lv27 <elvin.gm27@gmail.com>
2018-01-20 16:08:06 +0100

    progress: added react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React  from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 
+import Home from './Home';
+import NotFound from './NotFound';
 import Challenge from './Challenge';
 import NavMenu from './NavMenu';
 
-class App extends Component {
-  render() {
-    return (
-        <Challenge />
-    );
-  }
-}
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/challenges" component={Challenge} />
+        <Route component={NotFound} />
+      </Switch>
+      <NavMenu />
+    </div>
+
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NotFound = () => (
+
+    <div className="not-found" />
+)
+
+export default NotFound
